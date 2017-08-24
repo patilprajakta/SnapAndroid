18cda8c958735e656a4a9c940997e5027115d7d9
Snehal-Tembare <snehal.tembare@synerzip.com>
2017-08-24 13:29:27 +0530

    First commit

---
+++ app/src/main/java/com/example/synerzip/snapx360/LogInActivity.java
@@ -0,0 +1,18 @@
+package com.example.synerzip.snapx360;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Snehal Tembare on 24/8/17.
+ * Copyright © 2017 Synerzip. All rights reserved
+ */
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/com/example/synerzip/snapx360/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.synerzip.snapx360;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Snehal Tembare on 24/8/17.
+ * Copyright © 2017 Synerzip. All rights reserved
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
